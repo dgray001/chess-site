01a226bb884e26d6eaf634e444b81b195708e66a
Yo-Dan <yodan.and.the.sixth.character@gmail.com>
2024-06-22 10:11:13 -0400

    update config for test

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/chess-site/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/chess-site/' : '/chess-site/'
 }
